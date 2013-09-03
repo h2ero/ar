ae14ab3aff2f8e9d167ab650d427150fc8a2846a
h2ero <122750707@qq.com>
2013-09-03 15:40:25 +0800

    remove comment in test

--- test.go
+++ test.go
@@ -3,21 +3,55 @@ import(
     "h2eroUtil/ar"
     "fmt"
 )
+func t(s string, num int) {
+    fmt.Println("/--------------------------------")
+    fmt.Println("")
+    fmt.Println(s)
+    fmt.Println("")
+}
 func main() {
-    // activeRecord.New()
-    // test.init()
+    num := 0
+    //test 1.
     test := ar.New()
-    // test.Select("id","name").From("table1").Build()
-    // fmt.Println(test.Sql)
-    // test.Select(ar.Expr("sum(id)"),"name").From("table1").Where("id", ">", ar.Expr("id")).
-    // WhereOpen().Where("id", "=", ar.Expr("id % 2")).Where("name", "=", "h2ero").Where("login_date", "=", "20120101").WhereClose().Build()
-    // OrWhere("id", "<", "10").
-    // OrWhereOpen().Where("id", "=", "33"). Where("name", "=", "h2eros").WhereClose().
-    // Build()
     test.Select("id", "name").From("t1", "t2").Join("t3", "LEFT").On("t2.id", "=", ar.Expr("t3.uid % 2")).On("t2.id", "=", "t3.uid").Where("id", "=", "100").Build()
-    // // test.Select("id").From("t1", "t2").Where("id", "=", "1").OrderBy("id", "DESC").Limit(1,10).Offset(10).Build()
-    // // test.Insert("t1", []string{"name", "date"}).Values([]string{"h2ero", "1990"}).Build();
-    // test.Update("t1").Set(map[string]interface{}{"name":"h2ero", "cash":25, "topup":ar.Expr("topup-20")}).Where("id", "=", "3").Build()
-    // // test.Delete("t1").Where("id", "=", "3").Build()
-    fmt.Println(test.Sql)
+    t(test.Sql, num)
+
+    //test 2.
+    test = ar.New()
+    test.Select("id","name").From("table1").Build()
+    t(test.Sql, num)
+
+    //test 3.
+    test = ar.New()
+    test.Select(ar.Expr("sum(id)"),"name").From("table1").Where("id", ">", ar.Expr("id")).Build()
+    t(test.Sql, num)
+
+    //test 4.
+    test = ar.New()
+    test.Select(ar.Expr("sum(id)"),"name").From("table1").
+    Where("id", ">", ar.Expr("id")).
+    WhereOpen().Where("id", "=", ar.Expr("id % 2")).Where("name", "=", "h2ero").Where("login_date", "=", "20120101").WhereClose().
+    OrWhere("id", "<", "10").OrWhereOpen().Where("id", "=", "33"). Where("name", "=", "h2eros").WhereClose().
+    Build()
+    t(test.Sql, num)
+
+    //test 5.
+    test = ar.New()
+    test.Select("id").From("t1", "t2").Where("id", "=", "1").OrderBy("id", "DESC").Limit(1,10).Offset(10).Build()
+    t(test.Sql, num)
+
+    //test 6.
+    test = ar.New()
+    test.Insert("t1", []string{"name", "date"}).Values([]string{"h2ero", "1990"}).Build();
+    t(test.Sql, num)
+
+    //test 7.
+    test = ar.New()
+    test.Update("t1").Set(map[string]interface{}{"name":"h2ero", "cash":25, "topup":ar.Expr("topup-20")}).Where("id", "=", "3").Build()
+    t(test.Sql, num)
+
+    //test 8.
+    test = ar.New()
+    test.Delete("t1").Where("id", "=", "3").Build()
+    t(test.Sql, num)
 }
